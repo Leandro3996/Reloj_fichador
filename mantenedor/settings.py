2fc4410cdede71d6294f84214708ef095ebf98e9
Leandro3996 <nanitoelmejor96@gmail.com>
2025-11-11 10:19:30 -0300

    Implementación de SIMPLEUI

--- mantenedor/settings.py
+++ mantenedor/settings.py
@@ -389,3 +389,4 @@ SIMPLEUI_ICON = {
 SIMPLEUI_HOME_TITLE = 'Sistema de Control de Asistencia'  # Título de la página de inicio
 SIMPLEUI_HOME_ICON = 'fas fa-home'  # Icono de inicio
 SIMPLEUI_LOGO = '/static/images/logo_hores.png'  # Logo personalizado
+SIMPLEUI_FAVICON = 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⏰</text></svg>'  # Favicon emoji reloj
